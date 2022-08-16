a22d1d2e56ec398d9c3d143129f86c1eb7ccba4d
Zawadi <zawadi.opoti@student.moringaschool.com>
2022-08-16 14:19:51 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
+  
+ const multiply = num1 * num2;
+ 
+ const random = Math.floor(Math.random() * 10);
 
-// Write your code here
+const num3 = 16;
+const num4 = 6;
+
+ const mod = num3 % num4;
+
+ const max = Math.max(4, 5, 16, 20);
